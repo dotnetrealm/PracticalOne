54780ddf07b317892a5f13361978f33c7ad26e21
Bhavin-Kareliya <bhavin.k@simformsolutions.com>
2023-05-01 11:55:23 +0530

    Feat: repeat operation, appreciation message, etc...

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,27 +1,41 @@
 ﻿namespace Calculator
 {
-    internal class Program
+    internal static class Program
     {
         static void Main(string[] args)
         {
-            double num1 = 0, num2 = 0;
-
-            Console.Write("Please enter first number: ");
-            while (!Double.TryParse(Console.ReadLine(), out num1))
+            do
             {
-                Program.PrintError("[ERROR]: Value must numberic\n");
-                Console.Write("Please enter first number: ");
-            }
+                double num1 = 0, num2 = 0;
 
-            Console.Write("Please enter secound number: ");
-            while (!Double.TryParse(Console.ReadLine(), out num2) || num1.ToString().Length > 25)
-            {
-                Program.PrintError("[ERROR]: Value must numberic\n");
-                Console.Write("Please enter secound number: ");
-            }
-            Console.WriteLine();
-            Calculate(num1, num2);
+                #region User Inputs
+                Console.Write("\n\nPlease enter first number: ");
+                while (!Double.TryParse(Console.ReadLine(), out num1))
+                {
+                    PrintError("[ERROR]: Value-1 must numberic\n");
+                    Console.Write("Please enter first number: ");
+                }
+                Console.Write("Please enter second number: ");
+                while (!Double.TryParse(Console.ReadLine(), out num2) || num1.ToString().Length > 25)
+                {
+                    PrintError("[ERROR]: Value-2 must numberic\n");
+                    Console.Write("Please enter secound number: ");
+                }
+                Console.WriteLine();
+                #endregion User Inputs
+
+                Calculate(num1, num2);
+
+                Console.Write("\nDo you want to repeat again?(y/n): ");
+            } while (Console.ReadKey().KeyChar.ToString().ToLower() == "y");
+
+            #region Appreciations
+            PrintSuccess("\n\n*********************");
+            PrintSuccess("Thank you! \nPress any key to exit..");
+            PrintSuccess("*********************");
             Console.ReadKey();
+            #endregion Appreciations
+
         }
         static void Calculate(double num1, double num2)
         {
@@ -33,11 +47,11 @@
             try
             {
                 if (num2 == 0) throw new DivideByZeroException();
-                div = Math.Round(num1 / num2, 4);
+                div = num1 / num2;
             }
             catch (DivideByZeroException)
             {
-                Program.PrintError("[Exception]: Can't divide first number by zero(0).");
+                PrintError($"[Exception]: Can't divide {num1} by zero(0).");
             }
 
             PrintData((add, sub, mul, div), num1, num2);
@@ -56,10 +70,15 @@
             Console.WriteLine("=======================================");
             Console.ForegroundColor = ConsoleColor.White;
         }
-
         public static void PrintError(string err)
         {
             Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(err);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        public static void PrintSuccess(string err)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine(err);
             Console.ForegroundColor = ConsoleColor.White;
         }
